1fe7426b04990c3008de8f683e05c7cb18889195
delafuentej <jfuentelucas@gmail.com>
2024-07-01 10:21:56 +0200

    react-navigation installed & configured + stack navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+/* eslint-disable prettier/prettier */
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { Text, View } from 'react-native';
 
 export const App = () => {
   return (
-    <View>
-      <Text>MoviesApp</Text>
-    </View>
+    <NavigationContainer>
+      <View>
+        <Text>MoviesApp</Text>
+      </View>
+    </NavigationContainer>
   );
 };
