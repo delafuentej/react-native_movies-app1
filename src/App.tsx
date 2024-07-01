4734dbd69558cbafedc9368eee0d54010c162efa
delafuentej <jfuentelucas@gmail.com>
2024-07-01 12:07:25 +0200

    setting navigation in input file & environment variable added

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,12 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { Text, View } from 'react-native';
+import { Navigation } from './presentation/navigation/Navigation';
 
 export const App = () => {
   return (
     <NavigationContainer>
-      <View>
-        <Text>MoviesApp</Text>
-      </View>
+      <Navigation />
     </NavigationContainer>
   );
 };
